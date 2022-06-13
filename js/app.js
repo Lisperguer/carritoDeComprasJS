ccfcd6cf8eb252f543d006c49f4922a250020dc7
Felipe Lisperguer <felipe.lisperguer.h@gmail.com>
2022-06-13 03:53:46 -0400

    Now you can delete an item from the Carrito

--- js/app.js
+++ js/app.js
@@ -9,7 +9,9 @@ let articulosCarrito = [];
 cargarEventListeners();
 function cargarEventListeners(){
   // Cuando agregar un curso presionando "Agregar al carrito"
-  listaCursos.addEventListener('click', agregarCurso)
+  listaCursos.addEventListener('click', agregarCurso);
+
+  carrito.addEventListener('click', eliminarCurso)
 }
 
 //Funciones
@@ -20,6 +22,15 @@ function agregarCurso(e){
     const cursoSeleccionado = e.target.parentElement.parentElement;
     leerDatosCurso(cursoSeleccionado);
   }
+}
+
+//Funcion eliminar curso
+function eliminarCurso(e){
+  if(e.target.classList.contains('borrar-curso')){
+    const cursoId = e.target.getAttribute('data-id');
+    articulosCarrito = articulosCarrito.filter( curso => curso.id !== cursoId);
+  }
+  carritoHTML();
 }
 //Ahora extraeremos la información al hacer click para agregarla al carrito
 
@@ -34,13 +45,33 @@ function leerDatosCurso(curso){
     id: curso.querySelector('a').getAttribute('data-id'),
     cantidad: 1
   }
-  console.log(infoCurso)
+  // console.log(infoCurso)
+  //Revisaremos si un articulo ya existe en el carrito, si existe, solo actualiza la cantidad
+  const existe = articulosCarrito.some( curso => curso.id === infoCurso.id);
+  // console.log(existe);
+  if (existe){
+    //Actualizamos cantidad
+    const cursos = articulosCarrito.map( curso => {
+      if (curso.id===infoCurso.id){
+        curso.cantidad++;
+        return curso;
+      }else{
+        return curso;
+      }
 
+    });
+    console.log(cursos)
+    console.log('aqui')
+    articulosCarrito = [...cursos];
+    carritoHTML();
+
+  }else{
   //Agregar elemento a la lista con spread operator
   articulosCarrito = [...articulosCarrito, infoCurso];
-  console.log(articulosCarrito);
+  // console.log(articulosCarrito);
   carritoHTML();
-}
+
+}}
 
 //Muestra el carrito de compras en el HTML
 
@@ -56,11 +87,14 @@ function carritoHTML(){
       ${curso.titulo}
     </td>
     <td>
-    ${curso.precio}
-  </td>
-  <td>
-  ${curso.cantidad}
-</td>
+      ${curso.precio}
+    </td>
+    <td>
+      ${curso.cantidad}
+    </td>
+    <td>
+      <a href="#" class="borrar-curso" data-id= "${curso.id}"> X </a>
+    </td>
     `;
 
     //Agrega el HTML del carrito en el tbody
